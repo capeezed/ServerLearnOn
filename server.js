dd6f12b6ed09e0355e8818170a631c88f429483a
yukari4u <mkato4@gmail.com>
2025-10-19 14:51:57 -0300

    CORS att

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // Configura o CORS: permite que o frontend Angular (localhost:4200) se conecte
 app.use(cors({
-    origin: '*',
+    origin: 'https://learnonstartup.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
